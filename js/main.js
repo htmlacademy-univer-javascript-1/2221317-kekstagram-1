bea02df10dd91317e3b92e2483393725845eb281
Medvedeva-J <62373163+Medvedeva-J@users.noreply.github.com>
2022-11-04 18:37:44 +0500

    генерация постов и комментов

--- js/main.js
+++ js/main.js
@@ -1,11 +1,42 @@
+import {COMMENTS, NAMES, commentsIdList} from './info.js';
+
 function getRandomNumber(from, to) {
-    if (from < 0 || to < 0) return 'Диапазон может быть только положительным'
-    if (from > to) {            //При значении "от", большим, чем значение "до", функция переворачивает данный отрезок и всё равно возвращает псевдорандомное числов указанном диапазоне.
-        from, to = to, from     //При равных введённых значениях "от" и "до" функция вернёт это значение.
-    }
-    return Math.floor(Math.random() * (to - from + 1) + from);
+  if (from < 0 || to < 0) return 'Диапазон может быть только положительным'
+  if (from > to) {            //При значении "от", большим, чем значение "до", функция переворачивает данный отрезок и всё равно возвращает псевдорандомное числов указанном диапазоне.
+      from, to = to, from     //При равных введённых значениях "от" и "до" функция вернёт это значение.
+  }
+  return Math.floor(Math.random() * (to - from + 1) + from);
 }
 
 function checkStringLength(line, maxLength) {
-    return line.length <= maxLength;
+  return line.length <= maxLength;
+}
+//вот отсюда начинаются новые методы
+
+function createPhoto(id) {
+  let newPhoto = {
+      id: id,
+      url: `photos/${id}.jpg`,
+      description: 'Невероятно красивая картинка',
+      likes: getRandomNumber(15, 200),
+      comments: Array.from({length: getRandomNumber(1, 3)}, createComment)
+  };
+  return newPhoto;
+}
+
+function createComment() {
+  let newComment = {
+      id: commentsIdList.length,
+      avatar: `img/avatar-${getRandomNumber(1, 6)}.svg`,
+      message: COMMENTS[getRandomNumber(0, COMMENTS.length - 1)],
+      name: NAMES[getRandomNumber(0, NAMES.length - 1)]
+  }
+  commentsIdList.push(newComment.id);
+  return newComment;
+}
+
+const postsCount = 25;
+let postsList = [];
+for (let i = 1; i <= postsCount; i++) {
+  postsList.push(createPhoto(i))
 }
