5f254fd27d9379fc35ffc25b41de480936663bda
Keks <keks@htmlacademy.ru>
2022-11-28 20:24:46 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,10 @@
+import {createPhoto} from './util.js';
+import {createProfilePic, createFragment} from './draw.js';
+
+const postsCount = 25;
+const postsList = [];
+for (let i = 1; i <= postsCount; i++) {
+  postsList.push(createPhoto(i));
+}
+const picsArray = document.querySelector('.pictures');
+picsArray.append(createFragment(postsList, createProfilePic));
